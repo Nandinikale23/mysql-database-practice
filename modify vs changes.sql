0b2acf15a9349e2fb4dab5a6e633fd74d024d4f2
Nandinikale23 <nandinikale0623@gmail.com>
2026-02-12 07:13:12 +0530

    Add SQL examples for CHANGE vs MODIFY column operations

---
+++ modify vs changes.sql
@@ -0,0 +1,13 @@
+use startersql;
+
+-- ***changes vs modify column***:=  rename and change datatype 
+
+-- 1)change:=
+-- alter table users
+-- change column payment salary varchar(20);         -- rename payment to salary and chnage its datatype
+
+
+
+-- 2).modify
+-- alter table users
+-- modify column salary bigint;                     -- changes only datatype of salary 
